57d7b55aacbb4deaf99efdbee6e3ae7f78f2a043
Amalia Kivansa Anggreansi <72345090+amaliakivansa@users.noreply.github.com>
2024-11-11 13:49:12 +0700

    feat: add constants

---
+++ constants/catalog.js
@@ -0,0 +1,7 @@
+import book1 from '../assets/images/books/book-1.jpg';
+import book2 from '../assets/images/books/book-2.jpeg';
+import book3 from '../assets/images/books/book-3.jpeg';
+import book4 from '../assets/images/books/book-4.jpg';
+import book5 from '../assets/images/books/book-5.jpg';
+
+export default { book1, book2, book3, book4, book5 };
